229d9511f5f9a7d107ae90fdde7bf89dd52b1a41
Saifur Rahman Rubayet <saifur.imagic@gmail.com>
2026-04-27 11:05:10 +0600

    Add dist files

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+class PkIcons extends HTMLElement {
+  connectedCallback() {
+    const iconName = this.getAttribute('name');
+    this.innerHTML = this.getIcon(iconName);
+  }
+
+  getIcon(name) {
+    const icons = {
+      cross: `<svg viewBox="0 0 24 24"><path d="M6 6L18 18M6 18L18 6"/></svg>`,
+      check: `<svg viewBox="0 0 24 24"><path d="M5 13l4 4L19 7"/></svg>`
+    };
+
+    return icons[name] || '';
+  }
+}
+
+customElements.define('pkicons', PkIcons);
